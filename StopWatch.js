58f4322c6b844aabd6700c9624228664750eaca2
Rini-33 <130225149+Rini-33@users.noreply.github.com>
2023-04-15 19:50:16 +0530

    Commit No 1
    Uploading all the necessary files and trying to host it.

---
+++ StopWatch.js
@@ -0,0 +1,48 @@
+let seconds = 0;
+let minutes =0;
+let hours = 0;
+
+let start = document.getElementById('start'); 
+let pause = document.getElementById('stop');
+let reset = document.getElementById('reset'); 
+let stopstatus = 0;
+
+/* DOM ELEMENT FOR UPDATING TIMER */
+let timer = document.querySelector('.timer');
+
+/* TO START THE TIMER */
+start.addEventListener('click', ()=>{
+    if(stopstatus!==0){
+        clearInterval(stopstatus);
+    }
+    stopstatus = setInterval(()=>{
+        seconds+=1;
+        if(seconds === 60){
+            seconds = 0;
+            minutes++;
+            if(minutes === 60){
+                minutes = 0;
+                hours++;
+            }
+        }
+    let h = hours < 10 ? ("0" + hours ): hours;
+    let m = minutes < 10 ? ("0" + minutes) : minutes;
+    let s = seconds < 10 ?("0" + seconds) : seconds;
+    timer.innerHTML = ` ${h} : ${m} : ${s}`;
+},1000);
+});
+
+
+/* TO PAUSE THE TIMER */
+pause.addEventListener('click', ()=>{
+    clearInterval(stopstatus);
+});
+
+/* TO RESET THE TIMER */
+reset.addEventListener('click', ()=>{
+    clearInterval(stopstatus);
+    seconds = 0;
+    minutes =0;
+    hours = 0;
+    timer.innerHTML = '00 : 00 : 00 ';
+});
